fdfd751a67d4012b5bed63defe333d52d58c8649
bafang <ningfangxiao@futurevista.work>
2024-03-13 21:22:52 +0800

    first commit

---
+++ protocol/decoder.go
@@ -0,0 +1,25 @@
+package protocol
+
+import (
+	"bytes"
+	"encoding/json"
+	"errors"
+	"io"
+)
+
+func decodeBody(body io.Reader, v any) (err error) {
+	data, _ := io.ReadAll(body)
+	decoder := json.NewDecoder(bytes.NewReader(data))
+
+	if err = decoder.Decode(v); err != nil {
+		return err
+	}
+
+	_, err = decoder.Token()
+
+	if !errors.Is(err, io.EOF) {
+		return errors.New("The body contains trailing data")
+	}
+
+	return nil
+}
